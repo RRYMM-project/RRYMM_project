d838d054583f86831c108d03a0d0e5e8ec8c609f
Mariya-Vl <mariya.s.vlasova@gmail.com>
2025-02-13 07:55:24 +0200

    Cards margin was fixed. Pagination an navigation is on now

--- js/swiper.js
+++ js/swiper.js
@@ -1,17 +1,26 @@
-var swiper = new Swiper('.swiper',{
-    direction: 'horizontal',
-    loop: true,
+/* global Swiper */
 
-    pagination:{
-        el: '.swiping-pagination',
-    },
-
-    navigation:{
-        nextEl: 'swiper-button-next',
-        prevEl: 'swiper-button-prev',
-    },
-    slidesPerView:2,
-});
-
-// const swiper = document.querySelector('.swiper').swiper;
-// swiper.slideNext();
+document.addEventListener("DOMContentLoaded", function () {
+    if (typeof Swiper !== "undefined") {
+        const swiper = new Swiper('.swiper',{
+            direction: 'horizontal',
+            loop: true,
+        
+            pagination:{
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+        
+            navigation:{
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+                clickable: true,
+            },
+            slidesPerView:2,
+            spaceBetween: 30,
+            keyboard:true,
+        });     
+    } else {
+        console.error("Swiper didn't load");
+    }
+})
