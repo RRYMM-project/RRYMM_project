735d0ac530b9a401855e484fd47d1f07f1d07ad1
Mariya-Vl <mariya.s.vlasova@gmail.com>
2025-02-17 13:57:50 +0200

    JS-script created for building the section DevTools and Slider. Static data for devTools added

---
+++ js/data.js
@@ -0,0 +1,150 @@
+export const devTools = [
+    {
+      name: "HTML",
+      description:
+        "HyperText Markup Language (HTML) is the foundation of web development, used to structure the content of web pages. It defines elements like headings, paragraphs, lists, links, images, and more. HTML provides the basic layout and ensures that information is correctly displayed in the browser. Although it doesn't handle styling or interactivity, it works alongside CSS and JavaScript to create full-featured websites. Every web page starts with HTML, making it an essential tool for any developer.",
+      icon: "url('../images/icon-html.svg')",
+      iconWidth: "2.625rem",
+      usefulLinks: [
+        {
+          nameLink: "W3Schools tutorials:",
+          link: "https://www.w3schools.com/html/default.asp",
+        },
+        {
+          nameLink: "MDN resource:",
+          link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+        },
+        {
+          nameLink: "Codecademy:",
+          link: "https://www.codecademy.com/resources/docs/html",
+        },
+      ],
+    },
+    {
+      name: "CSS",
+      description:
+        "Cascading Style Sheets (CSS) is a styling language used to control the appearance and layout of web pages. It allows developers to customize fonts, colors, spacing, and responsive designs to ensure compatibility across devices. CSS separates content from presentation, improving code maintainability. Modern CSS includes advanced features like animations, grid layouts, and media queries, enhancing the user interface and experience.",
+      icon: "url('../images/icon-css.svg')",
+      iconWidth: "1.125rem",
+      usefulLinks: [
+        {
+          nameLink: "W3Schools tutorials:",
+          link: "https://www.w3schools.com/css/default.asp",
+        },
+        {
+          nameLink: "MDN resource:",
+          link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+        },
+        {
+          nameLink: "Codecademy:",
+          link: "https://www.codecademy.com/resources/docs/css",
+        },
+      ],
+    },
+    {
+      name: "SQL",
+      description: "Structured Query Language (SQL) is a standard language for managing relational databases. It allows developers to store, retrieve, update, and delete data efficiently. SQL is crucial for applications that handle large amounts of structured information, such as e-commerce sites and data-driven platforms. With SQL, users can perform complex queries, manage permissions, and maintain data integrity across multiple tables.",
+      icon: "",
+      iconWidth: "1.125rem",
+      usefulLinks: [
+        {
+          nameLink: "W3Schools tutorials:",
+          link: "https://www.w3schools.com/sql/default.asp",
+        },
+        {
+          nameLink: "Codecademy:",
+          link: "https://www.codecademy.com/resources/docs/sql",
+        },
+      ],
+    },
+    {
+      name: "Python",
+      description: "Python is a powerful and versatile programming language known for its clean syntax and ease of learning. It is widely used in web development, data analysis, machine learning, and automation. Python’s extensive libraries and frameworks, such as Django and Flask, simplify complex tasks. Its readability and broad application range make it a preferred language for beginners and experienced developers alike.",
+      icon: "",
+      iconWidth: "1.125rem",
+      usefulLinks: [
+        {
+          nameLink: "W3Schools tutorials:",
+          link: "https://www.w3schools.com/python/default.asp",
+        },
+        {
+          nameLink: "Codecademy:",
+          link: "https://www.codecademy.com/resources/docs/python",
+        },
+      ],
+    },
+    {
+      name: "GitHub",
+      description: "GitHub is a widely-used platform for version control and collaborative software development. Built around Git, it allows developers to track changes, collaborate on code, and manage multiple project versions. Teams use GitHub to review code, resolve issues, and automate workflows through continuous integration (CI). Its open-source ecosystem fosters innovation, while private repositories support secure development for businesses.",
+      icon: "",
+      iconWidth: "1.125rem",
+      usefulLinks: [
+        {
+          nameLink: "W3Schools tutorials:",
+          link: "",
+        },
+
+      ],
+    },
+    {
+      name: "Trello",
+      description: "Trello is a flexible project management tool based on a board-and-card system. It helps teams organize tasks, set deadlines, and track progress visually. Each board represents a project, with customizable lists and cards for individual tasks. Trello is popular for agile workflows, allowing users to prioritize work, assign responsibilities, and maintain an overview of ongoing projects. It integrates seamlessly with other productivity tools for better collaboration.",
+      icon: "",
+      iconWidth: "1.125rem",
+      usefulLinks: [
+        {
+          nameLink: "W3Schools tutorials:",
+          link: "",
+        },
+      ],
+    },
+    {
+      name: "Figma",
+      description: "Figma is a cloud-based design tool used for creating user interfaces (UI), prototypes, and wireframes. It allows real-time collaboration, enabling teams to work together on the same project from different locations. With its intuitive interface and powerful design capabilities, Figma streamlines the design-to-development process. It supports features like vector editing, component reuse, and interactive prototyping, making it a favorite among modern design teams.",
+      icon: "",
+      iconWidth: "1.125rem",
+      usefulLinks: [
+        {
+          nameLink: "W3Schools tutorials:",
+          link: "",
+        },
+      ],
+    },
+    {
+      name: "JavaScript",
+      description: "JavaScript is a powerful, dynamic programming language that adds interactivity and functionality to websites. It runs in the browser, allowing developers to create real-time updates, animations, form validations, and dynamic content without needing to reload the page. JavaScript works seamlessly with HTML and CSS, and its versatility extends to both client-side and server-side development. ",
+      icon: "",
+      iconWidth: "1.125rem",
+      usefulLinks: [
+        {
+          nameLink: "W3Schools tutorials:",
+          link: "https://www.w3schools.com/js/default.asp",
+        },
+        {
+          nameLink: "MDN resource:",
+          link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+        },
+        {
+          nameLink: "Codecademy:",
+          link: "https://www.codecademy.com/resources/docs/javascript",
+        },
+      ],
+    },
+    {
+      name: "PHP",
+      description: "PHP (Hypertext Preprocessor) is a popular server-side scripting language designed for web development. It enables the creation of dynamic, data-driven websites by interacting with databases, managing sessions, and processing user input. PHP is widely used in platforms like WordPress and content management systems (CMS). Its ability to generate HTML content on the server side makes it a critical part of web applications.",
+      icon: "",
+      iconWidth: "1.125rem",
+      usefulLinks: [
+        {
+          nameLink: "W3Schools tutorials:",
+          link: "https://www.w3schools.com/php/default.asp",
+        },
+        {
+          nameLink: "Codecademy:",
+          link: "https://www.codecademy.com/resources/docs/php",
+        },
+      ],
+    },
+    
+  ];
--- js/dev-tools.js
+++ js/dev-tools.js
@@ -1,91 +1,31 @@
-let devTools = [
-  {
-    name: "HTML",
-    description:
-      "HTML or Hypertext Markup Language is the standard markup language for documents designed to be displayed in a web browser. It defines the content and structure of web content.",
-    icon: "url('../images/icon-html.svg')",
-    iconWidth: "2.625rem",
-  },
-  {
-    name: "CSS",
-    description:
-      "Cascading Style Sheets is a style sheet language used for specifying the presentation and styling of a document written in a markup language such as HTML or XML.",
-    icon: "url('../images/icon-css.svg')",
-    iconWidth: "1.125rem",
-  },
-  {
-    name: "SQL",
-    description: "",
-    icon: "",
-    iconWidth: "1.125rem",
-  },
-  {
-    name: "Python",
-    description: "",
-    icon: "",
-    iconWidth: "1.125rem",
-  },
-  {
-    name: "GitHub",
-    description: "",
-    icon: "",
-    iconWidth: "1.125rem",
-  },
-  {
-    name: "Trello",
-    description: "",
-    icon: "",
-    iconWidth: "1.125rem",
-  },
-  {
-    name: "Figma",
-    description: "",
-    icon: "",
-    iconWidth: "1.125rem",
-  },
-  {
-    name: "JavaScript",
-    description: "",
-    icon: "",
-    iconWidth: "1.125rem",
-  },
-  {
-    name: "PHP",
-    description: "",
-    icon: "",
-    iconWidth: "1.125rem",
-  },
-];
+import { devTools } from "./data.js";
 
-console.log(devTools[0].name);
-console.log(devTools[1].description);
+// console.log(devTools[0].name);
+// console.log(devTools[1].description);
+// console.log(devTools.length);
+// console.log(devTools[0]["usefulLinks"][0].nameLink);
 
 function handleOnLoad(e) {
-    const container = createToolsSection();
-    document.getElementById("root").appendChild(container);
+    const toolsSection = createToolsSection();
+    document.getElementById("root").appendChild(toolsSection);
+    const sliderSection = createSliderSection();
+    document.getElementById("root").appendChild(sliderSection);
     
 }
 
 function createToolsSection() {
 
-    const toolsSectionDiv = document.createElement("div");
-    toolsSectionDiv.classList.add("container");
-    toolsSectionDiv.classList.add("tools-section");
+    const toolsSectionDiv = createDivWithClassContainer("tools-section");
 
-    const titleDiv = document.createElement("div");
-    titleDiv.classList.add("title-container");
-    const h2 = document.createElement("h2");
-    h2.classList.add("tools-title");
-    h2.innerText = "The magical word of programming";
-    titleDiv.appendChild(h2);
+    const titleDiv = createTitleDiv("The magical word of programming", null);
 
     const gridContainerDiv = document.createElement("div");
     gridContainerDiv.classList.add("tools-grid-container");
     devTools.forEach(tool => {
         const nameTool = tool.name;
-        console.log("Tools name is", nameTool);
+        // console.log("Tools name is", nameTool);
         const href = "#" + nameTool.toLowerCase();
-        console.log("Href is", href);
+        // console.log("Href is", href);
         let className = "grid-item";
         const gridItem = createGridItem(nameTool,href,className);
         gridContainerDiv.appendChild(gridItem);
@@ -94,10 +34,62 @@ function createToolsSection() {
     toolsSectionDiv.appendChild(titleDiv);
     toolsSectionDiv.appendChild(gridContainerDiv);
     
-    return toolsSectionDiv;
+    return toolsSectionDiv;    //AFTER SEARCH SECTION
 }
 
-// Function for creating div with teg a:
+function createSliderSection() {
+    const sliderDiv = createDivWithClassContainer("tools-slider-container");
+    const titleDiv = createTitleDiv("Let's explore developer tools together", "slider-title");
+
+    const swiper = createDivWithClassAndId("swiper", null);
+    const swiperWrapper = createDivWithClassAndId("swiper-wrapper", null);
+    
+    let indexTool = 0;
+    devTools.forEach(tool => {
+      // console.log(tool);
+      // console.log(indexTool);
+      const idValue = tool.name.toLocaleLowerCase();
+      const swiperSlide = createSwiperSlide(idValue,indexTool);
+      swiperWrapper.appendChild(swiperSlide);
+      indexTool = indexTool + 1; 
+    }
+    );   
+    swiper.appendChild(swiperWrapper);
+
+    const swiperBtnWrNext = createDivWithClassAndId("swiper-button-wrapper", null);
+    swiperBtnWrNext.classList.add("swiper-button-wrapper-next");
+    const swiperBtnNext = createDivWithClassAndId("swiper-button-next", null);
+    swiperBtnWrNext.appendChild(swiperBtnNext);
+    const swiperBtnWrPrev = createDivWithClassAndId("swiper-button-wrapper", null);
+    swiperBtnWrPrev.classList.add("swiper-button-wrapper-prev");
+    const swiperBtnPrev = createDivWithClassAndId("swiper-button-prev");
+    swiperBtnWrPrev.appendChild(swiperBtnPrev);
+    const swiperPagination = createDivWithClassAndId("swiper-pagination", null);
+
+    sliderDiv.appendChild(titleDiv);
+    sliderDiv.appendChild(swiper);
+    sliderDiv.appendChild(swiperPagination);
+    sliderDiv.appendChild(swiperBtnWrNext);
+    sliderDiv.appendChild(swiperBtnWrPrev);
+
+  
+    return sliderDiv; // AFTER toolsSectionDiv
+}
+
+// Function for creating div with class and ID:
+function createDivWithClassAndId(className="", id="") {
+  const div = document.createElement("div");
+  if (className){
+    div.classList.add(className);
+  }
+  if (id){
+    div.id = id;
+  }
+  
+  return div;
+}
+
+// Function for creating div with tag a:
 function createGridItem(nameTool, href, className) {
     const div = document.createElement("div");
     div.classList.add(className);
@@ -107,5 +99,98 @@ function createGridItem(nameTool, href, className) {
     div.appendChild(link);
     return div;
     };
+
+    // Function for creating div with class container:
+function createDivWithClassContainer(className) {
+    const div = document.createElement("div");
+    div.classList.add("container");
+    div.classList.add(className);
+    return div;
+}
+
+// Function for creating div with Title h2:
+function createTitleDiv(text,extraClass="") {
+    const titleDiv = document.createElement("div");
+    titleDiv.classList.add("title-container");
+    const h2 = document.createElement("h2");
+    h2.classList.add("tools-title");
+    if (extraClass){
+      h2.classList.add(extraClass);
+    }
+    h2.innerText = text;
+    titleDiv.appendChild(h2);
+    return titleDiv;
+}
+
+//Function for creating swiper slide:
+function createSwiperSlide(idValue, index) {
+  const swiperSlide = createDivWithClassAndId("swiper-slide", null);
+  const swiperCard = createDivWithClassAndId("slide-card", idValue);
+  
+  const cardLinks = createDivWithClassAndId("card-links", null);
+  const cardLinksWrapper = createDivWithClassAndId("card-links-wrapper");
+  const cardTitleH3 = document.createElement("h3");
+  cardTitleH3.classList.add("slide-card-title");
+  cardTitleH3.innerText = "Useful links";
+  cardLinksWrapper.appendChild(cardTitleH3);
+
+  //Creating section with source and links
+  const sourceList = document.createElement("ol");
+  sourceList.classList.add("card-link-list");
+
+  const devTool = devTools[index];
+  const sources = devTool.usefulLinks;
+  console.log(devTool);
+  const amountOfSources = sources.length;
+  // console.log("Amount of links:", amountOfSources);
+    
+  sources.forEach(source => {
+    const cardListItem = document.createElement("li");
+    cardListItem.classList.add("card-link-item");
+
+    const itemName = document.createElement("p");
+    itemName.classList.add("link-item-name");
+    itemName.innerText = source.nameLink;
+    cardListItem.appendChild(itemName);
+
+    const itemHref = document.createElement("a");
+    itemHref.setAttribute("target", "_blank");
+    const hrefLink = source.link;
+    itemHref.setAttribute("href", hrefLink);
+    itemHref.innerText = hrefLink; 
+    cardListItem.appendChild(itemHref);
+
+    sourceList.appendChild(cardListItem);
+    }); 
+
+  cardLinksWrapper.appendChild(sourceList);  
+  
+  const cardLinkBtn = document.createElement("button");
+  cardLinkBtn.classList.add("card-link-list-btn");
+  cardLinkBtn.innerText = "Learn more";
+  cardLinks.appendChild(cardLinksWrapper);
+  if (amountOfSources > 2) {
+    cardLinks.appendChild(cardLinkBtn);
+  }
+  
+  const cardDescript = createDivWithClassAndId("slide-card-descript", null);
+  const slideCardTitle = document.createElement("h3");
+  const iconExtraStyle = "icon-" + devTool.name.toLocaleLowerCase();
+  slideCardTitle.classList.add("slide-card-title", "icon", iconExtraStyle);
+  slideCardTitle.innerText = devTool.name;
+  const descriptText = document.createElement("p");
+  descriptText.classList.add("card-descript-text");
+  descriptText.innerText = devTool.description;
+  cardDescript.appendChild(slideCardTitle);
+  cardDescript.appendChild(descriptText);
+
+
+  swiperCard.appendChild(cardLinks);
+
+  swiperCard.appendChild(cardDescript);
+  swiperSlide.appendChild(swiperCard);
+
+  return swiperSlide;
+}
 
 addEventListener("load", handleOnLoad);
