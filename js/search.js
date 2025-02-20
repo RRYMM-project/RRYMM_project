7f8d8080c9c4b1d85b6b098edc9d0427ddcf3fca
Mariya-Vl <mariya.s.vlasova@gmail.com>
2025-02-20 17:37:09 +0200

    ::after and ::before for search-input-wrap fixed

--- js/search.js
+++ js/search.js
@@ -1,35 +1,34 @@
 function createSearchSection(params) {
-    const searchWrap = createDivWithClassAndId("search-wrap", null);
-    searchWrap.classList.add("container");
-    const searchInputWrap = createDivWithClassAndId("search-input-wrap", null);
-    const searchInput = document.createElement("input");
-    searchInput.setAttribute("type", "text");
-    searchInput.id = "search-content";
-    searchInput.setAttribute("placeholder", "Search");
-    searchInput.classList.add("search-input");
-    searchInputWrap.appendChild(searchInput);
-    searchWrap.appendChild(searchInputWrap);
+  const searchWrap = createDivWithClassAndId("search-wrap", null);
+  searchWrap.classList.add("container");
+  const searchInputWrap = createDivWithClassAndId("search-input-wrap", null);
+  const searchInput = document.createElement("input");
+  searchInput.setAttribute("type", "text");
+  searchInput.id = "search-content";
+  searchInput.setAttribute("placeholder", "Search");
+  searchInput.classList.add("search-input");
+  searchInputWrap.appendChild(searchInput);
+  searchWrap.appendChild(searchInputWrap);
 
-    return searchWrap;
+  return searchWrap;
 }
 
 function handleOnLoad(e) {
-    const searchSection = createSearchSection();
-    document.getElementById("root").appendChild(searchSection);
-    
+  const searchSection = createSearchSection();
+  document.getElementById("root").appendChild(searchSection);
 }
 
 // Function for creating div with class and ID:
-function createDivWithClassAndId(className="", id="") {
-    const div = document.createElement("div");
-    if (className){
-      div.classList.add(className);
-    }
-    if (id){
-      div.id = id;
-    }
-    
-    return div;
+function createDivWithClassAndId(className = "", id = "") {
+  const div = document.createElement("div");
+  if (className) {
+    div.classList.add(className);
   }
+  if (id) {
+    div.id = id;
+  }
+
+  return div;
+}
 
 addEventListener("load", handleOnLoad);
