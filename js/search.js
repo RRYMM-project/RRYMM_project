6a19c3505f3d5b1aa281df28b9fdd5d7e97fc8de
Mariya-Vl <mariya.s.vlasova@gmail.com>
2025-02-20 23:02:08 +0200

    Icon backspace changed from ::after to span. EventListener added to span

--- js/search.js
+++ js/search.js
@@ -1,13 +1,20 @@
-function createSearchSection(params) {
+function createSearchSection() {
   const searchWrap = createDivWithClassAndId("search-wrap", null);
   searchWrap.classList.add("container");
   const searchInputWrap = createDivWithClassAndId("search-input-wrap", null);
+  searchInputWrap.id = 'search-input-wrap';
   const searchInput = document.createElement("input");
   searchInput.setAttribute("type", "text");
   searchInput.id = "search-content";
   searchInput.setAttribute("placeholder", "Search");
   searchInput.classList.add("search-input");
+  searchInput.addEventListener('input', handleOnInput);
   searchInputWrap.appendChild(searchInput);
+  const backspaceIcon = document.createElement("span");
+  backspaceIcon.classList.add("material-symbols-outlined");
+  backspaceIcon.id = "backspace-icon";
+  backspaceIcon.innerText = "backspace";
+  searchInputWrap.appendChild(backspaceIcon);
   searchWrap.appendChild(searchInputWrap);
 
   return searchWrap;
@@ -32,3 +39,28 @@ function createDivWithClassAndId(className = "", id = "") {
 }
 
 addEventListener("load", handleOnLoad);
+
+function handleOnInput(e) {
+  const usersInput = e.target.value;
+  const bckspaceIcon = document.getElementById("backspace-icon");
+  console.log(bckspaceIcon);
+  if (usersInput != null) {
+    bckspaceIcon.classList.add("visible-icon");
+    bckspaceIcon.addEventListener('click', clearInputValue);
+  } 
+  if (usersInput == "") {
+    bckspaceIcon.classList.remove("visible-icon");
+    bckspaceIcon.removeEventListener('click', clearInputValue);
+  } 
+
+}
+
+function clearInputValue(e) {
+  console.log(e.target);
+  const element = e.target;
+  const elToClear = document.getElementById('search-content');
+  console.log(elToClear.value);
+  elToClear.value = "";
+  element.classList.remove("visible-icon");
+  
+}
